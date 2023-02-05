7e28e270e112ab332777c9a2acc845d541d21c9d
puleugo <puleugo@gmail.com>
2023-02-06 00:43:50 +0900

    feat: 소셜링 기능 스켈레톤 코드 구현

--- backend/src/app/community/social/social.controller.ts
+++ backend/src/app/community/social/social.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 
-@Controller('social')
-export class SocialController {}
+@Controller('socials')
+export class SocialController {
+  //TODO: 소셜링 구현
+
+  @Get()
+  async getSocials(): Promise<void> {
+    return;
+  }
+
+  @Get(':socialId')
+  async getSocialProfile(): Promise<void> {
+    return;
+  }
+
+  @Get(':socialId/invite-requests')
+  async getSocialInviteRequests(): Promise<void> {
+    return;
+  }
+
+  @Post()
+  async createSocial(): Promise<void> {
+    return;
+  }
+
+  @Post('join/:socialId')
+  async joinSocial(): Promise<void> {
+    return;
+  }
+
+  @Post(':socialId/invite-requests')
+  async createSocialInviteRequest(): Promise<void> {
+    return;
+  }
+
+  @Post('leave/:socialId')
+  async leaveSocial(): Promise<void> {
+    return;
+  }
+
+  @Post('kick/:socialId')
+  async kickSocial(): Promise<void> {
+    return;
+  }
+
+  @Patch(':socialId')
+  async updateSocial(): Promise<void> {
+    return;
+  }
+
+  @Delete(':socialId')
+  async deleteSocial(): Promise<void> {
+    return;
+  }
+}
